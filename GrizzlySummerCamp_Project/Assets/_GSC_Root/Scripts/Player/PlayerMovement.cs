d1fc48d18643e38a066baa7e561967419eabed8f
Capuchinochino <92971119+Capuchinochino@users.noreply.github.com>
2025-10-15 12:50:15 +0200

    Blocking Hub+Modificaciones de la cámara

--- GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Player/PlayerMovement.cs
+++ GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Player/PlayerMovement.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+//Importamos el InputSystem.
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Header = Titulo
     [Header("Move Stats")]
+    //SerializeField = Nombre de "x" valor, se mantiene privado pero se puede editar en el registro.
+    //float speed = Valor con decimales. [Tipo valor] [Nombre valor] [Cantidad valor(Sí se deja vació equivale a 0)]
     [SerializeField] float Speed;
 
+    //Player Input = El input es una función que recive valores y los traduce (teclado&ratón,mando,etc. a dirección,cantidad,etc) se usa para efectuar acciones
     PlayerInput  playerInput;
     InputAction moveAction;
     void Start()
@@ -20,9 +25,11 @@ public class PlayerMovement : MonoBehaviour
         MovePlayer();
     }
 
+    //Función que se llama para mover al player.
     void MovePlayer() 
     {
         Vector2 direction = moveAction.ReadValue<Vector2>();
+        //
         transform.position += new Vector3(direction.x, 0, direction.y) * Speed * Time.deltaTime;
     }
 }
