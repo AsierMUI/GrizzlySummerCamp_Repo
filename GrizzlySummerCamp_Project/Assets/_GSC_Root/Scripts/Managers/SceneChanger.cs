40146032ac016576a30e9b77dcfc78ac759f2798
AsierMUI <asieravino@gmail.com>
2025-10-15 16:53:49 +0200

    script scenechanger

---
+++ GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Managers/SceneChanger.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+   public void ChangeScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
