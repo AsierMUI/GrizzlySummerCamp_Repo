a558c09fcf812cf9316318905474d71cd88d3a85
Capuchinochino <92971119+Capuchinochino@users.noreply.github.com>
2025-10-16 11:38:00 +0200

    Mecánica de caminado+Cámara

--- GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Player/PlayerMovement.cs
+++ GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Player/PlayerMovement.cs
@@ -7,17 +7,25 @@ public class PlayerMovement : MonoBehaviour
     //Header = Titulo
     [Header("Move Stats")]
     //SerializeField = Nombre de "x" valor, se mantiene privado pero se puede editar en el registro.
-    //float speed = Valor con decimales. [Tipo valor] [Nombre valor] [Cantidad valor(Sí se deja vació equivale a 0)]
+    //float speed = Valor con decimales. [Tipo valor] [Nombre valor] [Cantidad valor(SÃ­ se deja vaciÃ³ equivale a 0)]
     [SerializeField] float Speed;
+    [SerializeField] float rotationSpeed;
+    [Space]
+    //float rotationSpeed = 0.2f;
+    [SerializeField] Rigidbody rb;
 
-    //Player Input = El input es una función que recive valores y los traduce (teclado&ratón,mando,etc. a dirección,cantidad,etc) se usa para efectuar acciones
-    PlayerInput  playerInput;
+    //Player Input = El input es una funciÃ³n que recive valores y los traduce (teclado&ratÃ³n,mando,etc. a direcciÃ³n,cantidad,etc) se usa para efectuar acciones
+    PlayerInput playerInput;
     InputAction moveAction;
-    void Start()
+    private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
+
+    }
+    void Start()
+    {
         moveAction = playerInput.actions.FindAction("Move");
-        
     }
 
     void Update()
@@ -25,11 +33,19 @@ public class PlayerMovement : MonoBehaviour
         MovePlayer();
     }
 
-    //Función que se llama para mover al player.
-    void MovePlayer() 
+    //FunciÃ³n que se llama para mover al player.
+    void MovePlayer()
     {
         Vector2 direction = moveAction.ReadValue<Vector2>();
-        //
-        transform.position += new Vector3(direction.x, 0, direction.y) * Speed * Time.deltaTime;
+        Vector3 moveDir = new Vector3(direction.x, 0, direction.y);
+
+        transform.position += moveDir * Speed * Time.deltaTime;
+
+        //Este cÃ³digo sirve para rotar al personaje, si su movimiento es mayor de 0.01.
+        if (moveDir.sqrMagnitude > 0.01f) 
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDir);
+            transform.rotation = Quaternion.Lerp(transform.rotation,targetRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 }
