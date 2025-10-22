921691261f2226f662241bb92fbffede80bb5de8
AsierMUI <asieravino@gmail.com>
2025-10-22 13:11:22 +0200

    interfaces

--- GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Managers/Temporizador.cs
+++ GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Managers/Temporizador.cs
@@ -4,7 +4,6 @@ using UnityEngine.UI;
 public class Temporizador : MonoBehaviour
 {
     [SerializeField] private float tiempoMax;
-   // [SerializeField] private GameObject[] sprites; //aqui ponemos los sprites de los numeros
     [SerializeField] private Slider slider;
     private float tiempoActual;
     private bool tiempoActivado = false;
@@ -61,16 +60,4 @@ public class Temporizador : MonoBehaviour
             ActivarTemporizador();
         }
     }
-
-    //public void IniciarAnimacion()
-   // {
-       // foreach (GameObject sprite in sprites)
-        //{
-          //  sprite.SetActive(true); //los sprites tienen q estar inactivos al principio
-          //  sprite.transform.localScale = Vector3.zero; //tiene el tamaño en 0
-         //   LeanTween.scale(sprite, Vector3.one, 1f).setEase(LeanTweenType.easeOutBounce);
-      // }
-      //  LeanTween.delayedCall(0.5f, () => ActivarTemporizador());
-    //}
-
 }
