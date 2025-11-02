aa536790d90e31fb397f9746e8bce4bd9b37b26c
AsierMUI <asieravino@gmail.com>
2025-11-02 14:09:59 +0100

    audio mixer

---
+++ GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Utilities/AudioManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Source")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("Audio Clip")]
+    public AudioClip background;
+    public AudioClip walking;
+    public AudioClip fishing;
+    public AudioClip boat;
+    public AudioClip bonk;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
