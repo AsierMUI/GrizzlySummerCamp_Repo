1ec4a1dbdafcc3f961a0ecabb6125c57d9ce2df1
AsierMUI <asieravino@gmail.com>
2025-11-05 10:25:27 +0100

    laberinto

---
+++ GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/MazeGenerator.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    [SerializeField] private MazeCell _mazeCellPrefab;
+    [SerializeField] private int _mazeWidth;
+    [SerializeField] private int _mazeDepth;
+
+    private MazeCell[,] _mazeGrid;
+
+    void Start()
+    {
+        _mazeGrid = new MazeCell[_mazeWidth, _mazeDepth];
+
+        for (int x = 0; x < _mazeWidth; x++)
+        {
+            for (int z = 0; z < _mazeDepth; z++)
+            {
+                _mazeGrid[x, z] = Instantiate(_mazeCellPrefab, new Vector3(x, 0, z), Quaternion.identity);
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
