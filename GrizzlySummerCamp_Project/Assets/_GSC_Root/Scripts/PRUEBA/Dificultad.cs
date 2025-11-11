e3c217e56d2a3eadf167473c1a9aca8ac9c94f85
AsierMUI <asieravino@gmail.com>
2025-11-11 21:26:20 +0100

    a la mierda los 4 fakin scripts

--- GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/PRUEBA/Dificultad.cs
+++
@@ -1,8 +0,0 @@
-   
-//Solo es un enum y no necesita estar en un gameobject, porque ya se define en los demas scripts
-public enum Dificultad
-    {
-        Facil,
-        Normal,
-        Dificil
-    }
--- GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/PRUEBA/EventManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System;
-
-public class EventManager : MonoBehaviour
-{
-    //maneja los eventos de fishcaught y escaped
-    public static Action<Dificultad> OnFishCaught;
-    public static Action OnFishEscaped;
-}
