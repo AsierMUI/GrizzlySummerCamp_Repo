4adfe9c3390f946da97b645466f2a72f27f5de33
AsierMUI <asieravino@gmail.com>
2025-11-24 12:08:13 +0100

    Movimiento de las boyas smootheado

--- GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/ScriptableObjects/MovimientoBoyas.cs
+++ GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/ScriptableObjects/MovimientoBoyas.cs
@@ -1,38 +1,72 @@
 using UnityEngine;
-using UnityEngine.Playables;
 
 public class MovimientoBoyas : MonoBehaviour
 {
     [SerializeField] private Transform[] waypoints;
-    [SerializeField] private float speed = 2f;
-
-    [Header("Opciones de movimiento")]
+    [SerializeField] private float speed = 1.5f;
     [SerializeField] private bool volverPorElMismoCamino = false;
 
     private int currentWaypointIndex = 0;
     private int direction = 1;
 
-    private void Update()
+    //Movimiento fluido
+    private float t = 0f;           //Progreso entre el waypont actual y el siguiente  
+    private float distance = 0f;    // distancia entre puntos
+    private Vector3 startPos;       // punto inicial
+    private Vector3 targetPos;      //punto final
+
+    private void Start()
     {
         if (waypoints.Length == 0) return;
 
-        Transform target = waypoints[currentWaypointIndex];
+        startPos = transform.position = waypoints[0].position;
+        SetNextTarget();
+    }
+    private void Update()
+    {
+        if (waypoints.Length <= 1) return;
 
-        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        //Avanza con la velocidad base del 0 al 1 da igual la distancia
+        t += (speed / distance) * Time.deltaTime;
 
-        if (Vector3.Distance(transform.position, target.position) < 0.05f)
+        //suavizado(aceleracion y desaceleracion)
+        float easedT = EaseInOut(t);
+
+        //Movimiento suave
+        transform.position = Vector3.Lerp(startPos, targetPos, easedT);
+
+        //Cuando llega al final del waypoint pasa al siguiente
+
+        if (t >= 1f)
+            SetNextTarget();
+    }
+
+    //Suavizado tipo ease in out,  t = 0 (lento), 0.5 (rapido), 1 (vuelve a frenar)
+
+    private float EaseInOut(float x)
+    {
+        return x * x * (3f - 2f * x);
+        //es una funcion de suavizado (smoothstep)
+    }
+
+    void SetNextTarget()
+    {
+        if (volverPorElMismoCamino)
         {
-            if (volverPorElMismoCamino)
-            {
-                if (currentWaypointIndex == waypoints.Length - 1)
-                    direction = -1;
-                else if (currentWaypointIndex == 0)
-                    direction = 1;
+            if (currentWaypointIndex == waypoints.Length - 1)
+                direction = -1;
+            else if (currentWaypointIndex == 0)
+                direction = 1;
 
-                currentWaypointIndex += direction;
-            }
-            else
-                currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+            currentWaypointIndex += direction;
         }
+        else
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+
+        startPos = transform.position;
+        targetPos = waypoints[currentWaypointIndex].position;
+
+        distance = Vector3.Distance(startPos, targetPos);
+        t = 0f;
     }
 }
