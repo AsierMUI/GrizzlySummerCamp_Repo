e45d4c310c2228c185069a28c607ce9e869a9935
AsierMUI <asieravino@gmail.com>
2025-12-05 18:48:21 +0100

    Shift para correr

--- GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Player/PlayerMovement.cs
+++ GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Player/PlayerMovement.cs
@@ -1,35 +1,27 @@
 using UnityEngine;
-//Importamos el InputSystem.
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Header = Titulo
     [Header("Move Stats")]
-    //SerializeField = Nombre de "x" valor, se mantiene privado pero se puede editar en el registro.
-    //float speed = Valor con decimales. [Tipo valor] [Nombre valor] [Cantidad valor(Sí se deja vació equivale a 0)]
     [SerializeField] float Speed;
     [SerializeField] float rotationSpeed;
     [SerializeField] Rigidbody rb;
-
-
     [SerializeField] float collisionSlowdown = 0.6f; //Entre 0.0-1.0 (0 a 100) 
     [SerializeField] float drag = 5f;
-
     [SerializeField] Animator animator;
-
     [SerializeField] ParticleSystem walkingVFX;
+    [SerializeField] float sprintMultiplier = 3f;
 
-    //Player Input = El input es una función que recive valores y los traduce (teclado&ratón,mando,etc. a dirección,cantidad,etc) se usa para efectuar acciones
     PlayerInput playerInput;
     InputAction moveAction;
+    InputAction sprintAction;
 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-
         rb.interpolation = RigidbodyInterpolation.Interpolate;
         rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
         rb.linearDamping = drag;
@@ -40,7 +32,8 @@ public class PlayerMovement : MonoBehaviour
     }
     void Start()
     {
-        moveAction = playerInput.actions["Move"]; 
+        moveAction = playerInput.actions["Move"];
+        sprintAction = playerInput.actions["Sprint"];
     }
 
     void FixedUpdate()
@@ -55,13 +48,17 @@ public class PlayerMovement : MonoBehaviour
         Vector3 moveDir = new Vector3(-input.x, 0, -input.y).normalized; //Código está en negativa "-input.x" para que mueva en dirección del mapa
 
         //animacion andar / idle
-        bool isWalking = moveDir.sqrMagnitude > 0.01;
+        bool isWalking = moveDir.sqrMagnitude > 0.01f;
         animator.SetBool("isWalking", isWalking);
-
         HandleWalkingVFX(isWalking);
 
+        //Comprueba si se pulsa sprint
+        float currentSpeed = Speed;
+        if (sprintAction.ReadValue<float>()> 0.1f)
+            currentSpeed *= sprintMultiplier;
+
         // Deseamos una velocidad en esa dirección
-        Vector3 desiredVelocity = moveDir * Speed;
+        Vector3 desiredVelocity = moveDir * currentSpeed;
         Vector3 velocityChange = desiredVelocity - rb.linearVelocity;   // Aplicamos cambio instantáneo de velocidad (como velocity pero moderno)
         rb.AddForce(velocityChange, ForceMode.VelocityChange);
 
