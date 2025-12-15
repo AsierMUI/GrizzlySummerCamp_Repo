355ca75ba424ec05419648e1944d98c08ef457e7
AsierMUI <asieravino@gmail.com>
2025-12-15 14:56:45 +0100

    CARPETA NUEVOS MANAGERS MINIJUEGOS

---
+++ GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Minigames/Estoy cansado jefe/ManagersPrueba/MinigameTimer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System;
+
+//SIRVE PARA TODOS LOS MINIJUEGOS
+
+public class MinigameTimer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
