20eb1ec4af3326ed479fa026a7acaa607cca773e
AsierMUI <asieravino@gmail.com>
2025-12-20 21:32:20 +0100

    Pruebas

--- GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Player/PlayerData.cs
+++ GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Player/PlayerData.cs
@@ -4,4 +4,11 @@ public class PlayerData : MonoBehaviour
 {
     public static Vector3 lastPosition = Vector3.zero;
     public static bool hasSavedPosition = false;
+
+    public static void SavePosition(Vector3 pos)
+    {
+        lastPosition = pos;
+        hasSavedPosition = true;
+        Debug.Log($"[PlayerData] Posicion guardada: {lastPosition}");
+    }
 }
--- GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Player/PlayerSpawnManager.cs
+++ GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Player/PlayerSpawnManager.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerSpawnManager : MonoBehaviour
 {
     void Start()
     {
+        if (SceneManager.GetActiveScene().name != "SCN_HUB")
+            return;
+
         if (PlayerData.hasSavedPosition)
         {
             transform.position = PlayerData.lastPosition;
