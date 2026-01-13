1ecc39823bbd9c2321bfa1b729b042f279f8eb81
AsierMUI <asieravino@gmail.com>
2026-01-13 15:51:53 +0100

    Script activador random funciona

--- GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Pruebas/RandomActivator.cs
+++ GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Pruebas/RandomActivator.cs
@@ -1,46 +1,53 @@
-/*using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class RandomActivator : MonoBehaviour
 {
     //Este script sirve tanto para pesca como para basura
-    [Header("Zona(Empty Padre)")]
-    public Transform zoneParent;
 
-    [Header("Cantidad a activar")]
-    public int minToActivate = 3;
-    public int maxToActivate = 3;
+    [System.Serializable]
+    public class Zone
+    {
+        public Transform zoneParent;
+        public int minToActivate = 3;
+        public int maxToActivate = 3;
+    }
 
-    private List<GameObject> children = new List<GameObject>();
+    [Header("Zonas del mapa")]
+    public List<Zone> zones = new List<Zone>();
 
     void Start()
     {
-        CacheChildren();
-        ActivateRandomChildren();
+        ActivateZones();
     }
 
-    private void CacheChildren()
+    public void ActivateZones()
     {
-        children.Clear();
-
-        foreach(Transform child in zoneParent)
+        foreach (Zone zone in zones)
         {
-            children.Add(child.gameObject);
+            if (zone.zoneParent == null) continue;
+
+            List<GameObject> children = new List<GameObject>();
+
+            foreach (Transform child in zone.zoneParent)
+            {
+                children.Add(child.gameObject);
+                child.gameObject.SetActive(false);
+            }
+
+            if (children.Count == 0) continue;
+
+            int amount = Random.Range(zone.minToActivate, zone.maxToActivate + 1);
+            amount = Mathf.Clamp(amount, 0, children.Count);
+
+            List<GameObject> tempList = new List<GameObject>(children);
+
+            for(int i = 0; i < amount; i++)
+            {
+                int randomIndex = Random.Range(0, tempList.Count);
+                tempList[randomIndex].SetActive(true);
+                tempList.RemoveAt(randomIndex);
+            }
         }
-    }
-
-    public void ActivateRandomChildren()
-    {
-        if (children.Count == 0) return;
-
-        foreach (GameObject obj in children)
-        {
-            obj.SetActive(false);
-        }
-
-        int amount = Random.Range(minToActivate, maxToActivate + 1);
-        amount = Mathf.Clamp(amount, 0, children.Count);
-
     }
 }
-*/
