11709def35c18d2991953156e1e1438a4d267062
AsierMUI <asieravino@gmail.com>
2026-01-20 20:59:40 +0100

    SFX Contrareloj

--- GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Minigames/Contrareloj/PickUpItem.cs
+++ GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Minigames/Contrareloj/PickUpItem.cs
@@ -2,9 +2,22 @@ using UnityEngine;
 
 public class PickUpItem : MonoBehaviour
 {
+    [Header("Audio")]
+    [SerializeField] private string pickupSfxKey;
+
+    private AudioManager audioManager;
+
+    private void Start()
+    {
+        audioManager = FindFirstObjectByType<AudioManager>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
+
+        if (!string.IsNullOrEmpty(pickupSfxKey))
+            audioManager?.PlaySFX(pickupSfxKey);
+
 
         PickUpManager.Instance?.CollectPickup(transform);
         Destroy(gameObject);
