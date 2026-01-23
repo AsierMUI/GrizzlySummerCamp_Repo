0490fec8b292febaea550e4fcccfe53d8661debc
AsierMUI <asieravino@gmail.com>
2026-01-23 15:55:25 +0100

    Se vienen cositas

---
+++ GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/JumpScareUI.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+
+public class JumpScareUI : MonoBehaviour
+{
+    public int clicksNecesarios = 5;
+    private int contadorClicks = 0;
+
+    public GameObject interfazSecreta;
+
+    [Header("Duracion jumpscare")]
+    public float duracionInterfaz = 3f;
+
+    [Header("Audio")]
+    public string keyJumpScareSFX;
+
+    private bool activado = false;
+    private AudioManager audioManager;
+
+    private void Awake()
+    {
+        audioManager = FindFirstObjectByType<AudioManager>();
+    }
+
+    public void BotonPresionado()
+    {
+        if (activado) return;
+
+        contadorClicks++;
+
+        if (contadorClicks >= clicksNecesarios)
+        {
+            activado = true;
+            StartCoroutine(ActivarEasterEgg());
+        }
+    }
+
+    IEnumerator ActivarEasterEgg()
+    {
+        interfazSecreta.SetActive(true);
+
+        if (audioManager != null && !string.IsNullOrEmpty(keyJumpScareSFX))
+            audioManager.PlaySFX(keyJumpScareSFX);
+
+        yield return new WaitForSeconds(duracionInterfaz);
+        Application.Quit();
+    }
+}
