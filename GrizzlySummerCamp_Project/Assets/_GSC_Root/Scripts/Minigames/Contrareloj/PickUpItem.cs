120d0f01ad464b43bdc833f6ffcf93bb4b411bcc
AsierMUI <asieravino@gmail.com>
2026-02-17 12:05:27 +0100

    Ahora ya si q va

--- GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Minigames/Contrareloj/PickUpItem.cs
+++ GrizzlySummerCamp_Project/Assets/_GSC_Root/Scripts/Minigames/Contrareloj/PickUpItem.cs
@@ -6,14 +6,14 @@ public class PickUpItem : MonoBehaviour
     [Header("Audio")]
     [SerializeField] private string pickupSfxKey;
 
+    [Header("Animator")]
+    [SerializeField] private Animator childAnimator;
+
     private AudioManager audioManager;
-    private Animator childAnimator;
 
     private void Start()
     {
         audioManager = FindFirstObjectByType<AudioManager>();
-
-        childAnimator = GetComponentInChildren<Animator>(true);
     }
 
     private void OnTriggerEnter(Collider other)
